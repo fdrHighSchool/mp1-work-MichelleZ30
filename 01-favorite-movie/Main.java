e46aa9762c03ca57f3a825793b23698b542da88c
AP Java Period 1 <apjavaperiod1@iss-jcoltrinari.78b505.nycboe.org>
2022-10-04 08:39:33 -0400

    fav movie final

---
+++ 01-favorite-movie/Main.java
@@ -0,0 +1,14 @@
+
+/**
+ * Favorite Movies repost accidently deleted
+ *
+ * @Michlle Zheng
+ * @10/4/2022
+ */
+class Main{
+  public static void main(String[] args) {
+    System.out.println("Last year me and my friends went to the movies to watch the new Marvel movie called, Enternals. It was very interesting and different from other Marvel movies that I have watched. I really recommend the movie if you are a Marvel or a DC fan. I also really liked the humor they used within the scenes and references to world history.");
+    System.out.print("The Harry Potter movies series are really good too, if you liked the book. You could also watch it with your kids and read the books with them. It is really fun too read book and watch the movies. I got hooked on the books when I first read it.");
+    System.out.println("There is a lot of like Barbie movies you could watch with your daughter if you don't mind girly movies. Those are some memories from my childhood and every movies is so pretty.");
+  }
+}
